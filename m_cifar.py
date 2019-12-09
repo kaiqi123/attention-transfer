7f37314bf313ea0b011df13af12282d6c596f1ab
kaiqi123 <kzhao27@asu.edu>
2019-12-09 16:50:49 -0700

    1209

--- m_cifar.py
+++ m_cifar.py
@@ -173,6 +173,7 @@ def main():
                     y_t, g_t = f_t(inputs, params, False, 'teacher.')
                 return y_s, y_t, [utils.at_loss(x, y) for x, y in zip(g_s, g_t)]
         elif opt.kt_method == "st":
+            print('Set st:')
             relu_out_s = {'student.' + k: v for k, v in relu_out_s.items()}
             relu_out_t = {'teacher.' + k: v for k, v in relu_out_t.items()}
             for key, value in relu_out_s.items(): print(key, value)
